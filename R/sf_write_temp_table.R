ffce116e000cc286417edf158f07bd3b1b7fe8cf
ghbruno <51383513+ghbruno@users.noreply.github.com>
2022-01-06 14:47:50 -0500

    Update sf_write_temp_table.R
    Now its possible to insert more than 16k rows

--- R/sf_write_temp_table.R
+++ R/sf_write_temp_table.R
@@ -1,9 +1,9 @@
 sf_write_temp_table<- function(con, df, table_name, overwrite=FALSE) {
-
+  
   
   data_types_transalation<-data.frame(data_type=c('integer','character', 'numeric', 'Date', 'POSIXct', 'POSIXt'),
                                       sql_data_type=c('int','varchar(255)','float','date','datetime','datetime'))
-
+  
   colnames(df)<-toupper(colnames(df))
   
   table_name<-toupper(table_name)
@@ -14,13 +14,21 @@ sf_write_temp_table<- function(con, df, table_name, overwrite=FALSE) {
   
   data_type_df<-merge(data.frame(columns=names(data_type),data_type), data_types_transalation, on = "data_type")
   
-  values<-paste("('",paste(apply(df,1,paste,collapse="','"), collapse="'),('"),"')",sep='')
-  
-  
   if(overwrite==TRUE) {RJDBC::dbSendQuery(con, paste("drop table if exists",table_name,";"))}
-
+  
   RJDBC::dbSendQuery(con,paste("CREATE TEMP TABLE ", table_name," (",paste(paste(data_type_df$columns,data_type_df$sql_data_type), collapse=','),");"))
-  RJDBC::dbSendQuery(con, paste("insert into ", table_name," (",paste(columns,collapse=','),") values ",values,";"))
+  
+  n_iter<-ceiling(nrow(df)/15000)
+  for(i in 1:n_iter) {
+  
+    indexes<-(1:15000)+(15000*(i-1))
+    indexes<-indexes[indexes<=nrow(df)]
+  
+    values<-paste("('",paste(apply(df[indexes,,drop=FALSE],1,paste,collapse="','"), collapse="'),('"),"')",sep='')
+    RJDBC::dbSendQuery(con, paste("insert into ", table_name," (",paste(columns,collapse=','),") values ",values,";"))
+  
+  }
+  
   cat("TABLE'S DESCRIPTION\n")
   print(RJDBC::dbGetQuery(con,paste("DESC TABLE ",table_name,";",sep='')))
   cat("\n\n\n 10 OBS SAMPLE\n")
